2760a4c2a70feaa08ddca5c1861c939509008528
eivicent <vicent.boned.11@gmail.com>
2022-09-20 23:43:08 +0300

    Getting somewhere with pins

--- hki_apartment_analysis.R
+++ hki_apartment_analysis.R
@@ -16,7 +16,6 @@ clean_barris_apartments_df <- function(apartments_df, dim_barris = NULL){
            dplyr::rename(barri = actualitzat),.) %>%
     dplyr::distinct()
 }
-
 count_barris_progressio <- function(path, dim_barris = dim_barris){
   all_data <- lapply(list.files(path), function(x) 
     pins::pin_read(pins::board_folder(path = path),
@@ -39,11 +38,11 @@ tendencia_general <- function(path){
   out <- tibble(date = names(trend), general_trend = trend)
   return(out)
 }
-
-ultimes_dades_disponibles <- function(path, minim_barri = 2, minim_preu = 4000){
+ultimes_dades_disponibles <- function(path, minim_barri = 2, minim_preu = 4000, dim_barris = dim_barris){
   all_data <- pins::pin_read(pins::board_folder(path = path),
                    name = max(list.files(path))) %>%
-    pluck("results_df")
+    pluck("results_df") %>%
+    clean_barris_apartments_df(., dim_barris)
   
   out <- all_data %>%
     group_by(barri) %>%
@@ -57,10 +56,12 @@ pinta_distribucio_barri <- function(df){
   df %>%
     ggplot(aes(x = fct_reorder(barri, desc(barri)),
                y = preu_metre)) +
-    geom_boxplot(outlier.shape = NA) +
+    geom_boxplot(outlier.shape = NA, coef = 1, linetype = 1, 
+                 color = "blue") +
+    geom_boxplot(outlier.shape = NA, fatten = 5, coef = 0) +
     geom_jitter(alpha = .1) +
     coord_flip() +
-    geom_text(data = . %>% select(barri, count_barri) %>% unique, 
+    geom_text(data = df %>% select(barri, count_barri) %>% unique, 
               aes(x = barri,  y = 3.6e3, label = count_barri)) +
     scale_y_continuous(breaks = seq(4.5,20e3, by = 1.5e3),
                        labels = scales::dollar_format(accuracy = .1,
@@ -79,53 +80,75 @@ pinta_distribucio_barri <- function(df){
 last_date <- list.files(path) %>% max()
 barris <- count_barris_progressio(path, dim_barris)
 tendencia <- tendencia_general(path)
-df <- ultimes_dades_disponibles(path, minim_barri = 4) %>%
-  clean_barris_apartments_df(., dim_barris)
+df <- ultimes_dades_disponibles(path, minim_barri = 4, dim_barris = dim_barris) 
 
 tendencia_barris <- barris %>% 
   group_by(barri) %>% arrange(date) %>%
   mutate(diff_n = (n - coalesce(lag(n),n))/n,
          diff_preu = (avgpreu-coalesce(lag(avgpreu),avgpreu))/avgpreu) %>%
-  slice_tail(n = 1)
+  slice_tail(n = 1) %>% ungroup()
 
-tendencia_barris %>% 
-  ggplot(aes(x = 1, y = 1, label = diff_n)) +
-  geom_text() +
-  facet_wrap("barri")
+tendencia_barris %>% arrange(desc(n)) %>%
+  slice_head(n=4) %>%
+  ggplot(aes(x = avgpreu, y = 1, label = round(avgpreu,1))) +
+    geom_linerange(aes(xmin = minpreu, xmax = maxpreu), alpha =.3) +
+    geom_text() +
+    facet_wrap("barri") +
+    theme_void(base_size = 16)
 
+
+
+### SHINY APP
 
 ui <- fluidPage(
+  # fluidRow(
+  #   plotOutput("distplot_plot", dblclick = "f_barri_2")
+  #   ),
+  # fluidRow(
+  #   tableOutput("dobleclick"),
+  #   checkboxInput(inputId = "s_barri",label = "Select all", value = T),
+  #   uiOutput("ui_f_barri"),
+  #   plotOutput("scatter_plot",brush = "plot_brush")
+  #         ),
   fluidRow(
-    valueBox(mean(df$preu_metre),subtitle = "")
+    column(width = 3,
+           actionButton(inputId = "interested_button",label = "Interested")
+    ),
+    column(width = 3,
+           actionButton(inputId = "not_interested_button",label = "Not interested")
+    ),
+    column(width = 3,
+           actionButton(inputId = "seen_liked_button",label = "Seen-and-liked")
+    ),
+    column(width = 3,
+           actionButton(inputId = "seen_not_liked_button",label = "Seen-NO")
+    )
   ),
   fluidRow(
-    plotOutput("distplot_plot", dblclick = "f_barri")
+    DT::dataTableOutput("main_table")
     ),
   fluidRow(
-    plotOutput("scatter_plot",brush = "plot_brush")
-          ),
-  fluidRow(
-    DT::dataTableOutput("main_table")
-    )
+    DT::dataTableOutput("text")
+  )
 )
 
 server <- function(input, output, session) {
-
-
+  output$ui_f_barri <- renderUI({
+    if(!input$s_barri){
+      selectInput(inputId = "f_barri", label = "barri", choice = unique(df$barri),
+                  selected = nearPoints(df,input$f_barri_2, yvar= "barri") %>%
+                    unique() %>%
+                    pull(barri))
+    }
+  })
   output$distplot_plot <- renderPlot({
     pinta_distribucio_barri(df)
     })
-  
-  output$trendplot_plot <- renderPlot({
-    ggplot(df %>% filter(barri == input$f_barri),
-           aes(x = preu_metre)) +
-      geom_bar(stat = "count")
-    
-  })
-  
   data <- reactive({
+    if(!input$s_barri){
     df %>%
       dplyr::filter(barri %in% input$f_barri)
+    } else {df}
   })
   
   output$scatter_plot <- renderPlot({
@@ -142,19 +165,91 @@ server <- function(input, output, session) {
       theme_minimal(base_size = 14)
   })
   
-  output$main_table <- DT::renderDataTable({
+  brushed_data <- reactive({
     data <- brushedPoints(data(), input$plot_brush,allRows = T) %>%
       mutate(id = paste0("<a href = \"",id, "\"> ",id,"</a>"))
-    
-    if(sum(data$selected_) == 0){
-      out <- data %>% select(-selected_)
-    } else { 
+     
+     if(sum(data$selected_) == 0){
+     out <- data %>% select(-selected_)
+    } else {
       out <- filter(data, selected_==T) %>% select(-selected_)
     }
-    DT::datatable(out, escape = F,rownames = F)})
+      return(data)
+  })
+  
+  output$main_table <- DT::renderDataTable({
+    DT::datatable(df, escape = F,rownames = F)},server = T)
+  
+  output$text <- DT::renderDataTable(data_to_update())
+  
+  # observeEvent(input$interested_button,{
+  #              aux <- df[input$main_table_rows_selected,]  %>%
+  #                select(id) %>%
+  #                mutate(status = "interested")
+  #              
+  #              out <- rows_update(df, aux)
+  #               } 
+  #              )
+  # observeEvent(input$intersted_button, {
+  #   
+  #   aux <- df[input$main_table_rows_selected,]  %>%
+  #     select(id) %>%
+  #     mutate(status = "interested")
+  #   
+  #   out <- left_join(update_categorized_df, aux) %>%
+  #     filter(!is.na(status))
+  #   
+  #   pins::pin_write(pins::board_folder(path = path),
+  #                  name = "categorized", x = out,versioned = F,type = "csv",
+  #                  title = "Categorized apartments")
+  #   return(out)
+  # })
+  # observeEvent(input$intersted_button, {
+  #   
+  #   aux <- df[input$main_table_rows_selected,]  %>%
+  #     select(id) %>%
+  #     mutate(status = "not_interested_button")
+  #   
+  #   out <- left_join(update_categorized_df, aux) %>%
+  #     filter(!is.na(status))
+  #   
+  #   pins::pin_write(pins::board_folder(path = path),
+  #                   name = "categorized", x = out,versioned = F,type = "csv",
+  #                   title = "Categorized apartments")
+  #   return(out)
+  # })
+  # observeEvent(input$intersted_button, {
+  #   
+  #   aux <- df[input$main_table_rows_selected,]  %>%
+  #     select(id) %>%
+  #     mutate(status = "interested")
+  #   
+  #   out <- left_join(df, aux) %>%
+  #     filter(!is.na(status))
+  #   
+  #   pins::pin_write(pins::board_folder(path = path),
+  #                   name = "categorized", x = out,versioned = F,type = "csv",
+  #                   title = "Categorized apartments")
+  #   return(out)
+  # })
+  # observeEvent(input$intersted_button, {
+  #   
+  #   aux <- df[input$main_table_rows_selected,]  %>%
+  #     select(id) %>%
+  #     mutate(status = "interested")
+  #   
+  #   out <- left_join(df, aux) %>%
+  #     filter(!is.na(status))
+  #   
+  #   pins::pin_write(pins::board_folder(path = path),
+  #                   name = "categorized", x = out,versioned = F,type = "csv",
+  #                   title = "Categorized apartments")
+  #   return(out)
+  # })
   
 }
 
 shinyApp(ui, server)
+
 
 
